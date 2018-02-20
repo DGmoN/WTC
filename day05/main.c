be8a5dbaabf9dd98b6a9846619d87f0cc0cc8c4c
wgourley <wgourley@student.wethinkcode.co.za>
2018-02-20 12:00:56 +0200

    11

--- day05/main.c
+++ day05/main.c
@@ -3,8 +3,8 @@
 #include "ex10/ft_str_is_lowercase.c"
 
 int main(){
-	char *str1 = "asdfghAasd";
-	char *str2 = "notAAAAAA";
+	char *str1 = "ASDFGHlKL";
+	char *str2 = "asdfghjkl";
 
 	printf("%i\n", ft_str_is_lowercase(str1));
 	printf("%i\n", ft_str_is_lowercase(str2));
